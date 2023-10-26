146aea877ed8b004291852677a58c0f5bcbf1f59
vikasseervi357 <vikasseervi357@gmail.com>
2023-10-26 17:43:46 +0530

    sec 2 ver 3

--- springcoredemo/src/main/java/com/vikas/springcoredemo/rest/DemoController.java
+++ springcoredemo/src/main/java/com/vikas/springcoredemo/rest/DemoController.java
@@ -9,19 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-
+/*
     // define a private field for the dependency
     private Coach myCoach;
+    private Coach anotherCoach;
     //@Autowired seeks for an appropriate class for the Coach(Interface)
     //It also tells Spring to inject a dependency
     //If we have only one constructor then @Autowired on constructor is optional
 
     // -> This is call Constructor Injection
     @Autowired
-    public DemoController(@Qualifier("hockeyCoach") Coach theCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach, @Qualifier("cricketCoach") Coach theAnotherCoach) {
+        System.out.println("In Controller: "+getClass().getSimpleName());
         myCoach = theCoach;
+        anotherCoach = theAnotherCoach;
     }
-
 
     // This is called Setter Injection
     /*
@@ -29,9 +31,22 @@ public class DemoController {
     public void setCoach(Coach theCoach){
         myCoach = theCoach;
     }
-     */
+ */
+//*********************************************************************************************************************************************************
+    private Coach myCoach;
+    private Coach anotherCoach;
+    @Autowired
+    public DemoController(@Qualifier("aquatic") Coach theCoach){
+        myCoach = theCoach;
+    }
     @GetMapping("/dailyworkout")
     public  String getDailyWorkout() {
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "Comparing beans: myCoach == anotherCoach => Objects are pointing to "+((myCoach == anotherCoach)
+                                            ?"same reference" : "different reference");
     }
 }

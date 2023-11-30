427d346de197168d4655393ab55e5c05517d7b2e
vikasseervi357 <vikasseervi357@gmail.com>
2023-11-30 15:29:19 +0530

    sec 5 ver 3

--- thymleaf/src/main/java/com/vikas/thymleaf/controller/StudentController.java
+++ thymleaf/src/main/java/com/vikas/thymleaf/controller/StudentController.java
@@ -1,18 +1,32 @@
 package com.vikas.thymleaf.controller;
 
 import com.vikas.thymleaf.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.security.PrivateKey;
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+    @Value("${languages}")
+    private List<String> languages;
+    @Value("${systems}")
+    private List<String> systems;
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
         Student theStudent = new Student();
         theModel.addAttribute("student", theStudent);
+        theModel.addAttribute("countries", countries);
+        theModel.addAttribute("languages", languages);
+        theModel.addAttribute("systems", systems);
         return "student-form";
     }
 
